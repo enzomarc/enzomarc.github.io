d70cac00436f5f077111e250083c25d0868b0808
Marc Enzo <emarc237@gmail.com>
2019-09-19 07:00:38 +0100

    New style

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the portfolio"; //letter subject
+	$to = 'emarc237@gmail.com'; // Enter Your E-mail
+	$from = 'emarc237@gmail.com'; // Admin e-mail
+	$fromName = 'Licon'; // Your company name
+	$charset = 'UTF-8';
+?>
